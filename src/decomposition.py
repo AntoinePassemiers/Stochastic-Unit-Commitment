Bug fix: Minimum down time overflow

src/decomposition.py
@@ -102,7 +102,8 @@ def decompose_problem(instance, mu, nu):
         for g in Gf:
             DTg = int(DT[g])
             for t in range(0, N-DTg-1):
-                problem += (np.sum(v[g, s, t+1:t+DTg+1]) <= 1 - u[g, s, t])
+                if t + 1 < T:
+                    problem += (np.sum(v[g, s, t+1:t+DTg+1]) <= 1 - u[g, s, t])
 
         # Define constraints group 3.34
         #    v[g, s, t] <= 1 for slow generators
@@ -145,8 +146,9 @@ def decompose_problem(instance, mu, nu):
     problem.set_constraint_group("3.30")
     for g in Gs:
         DTg = int(DT[g])
-        for t in range(0, N-DTg-1):
-            problem += (np.sum(z[g, t+1:t+DTg+1]) <= 1 + w[g, t])
+        for t in range(0, N-DTg+1):
+            if t + 1 < T:
+                problem += (np.sum(z[g, t+1:t+DTg+1]) <= 1 - w[g, t])
 
     # Define constraints group 3.33
     #    z[g, t] <= 1 for slow generators

src/lp_relaxation.py
@@ -108,8 +108,9 @@ def create_formulation(instance, lower_bound=None, relax=True):
     problem.set_constraint_group("3.30")
     for g in Gs:
         DTg = int(DT[g])
-        for t in range(0, N-DTg-1):
-            problem += (np.sum(z[g, t+1:t+DTg+1]) <= 1 + w[g, t])
+        for t in range(0, N-DTg+1):
+            if t + 1 < T:
+                problem += (np.sum(z[g, t+1:t+DTg+1]) <= 1 - w[g, t])
     
     # Define contraints group 3.31
     #    sum_{t-UT[g]+1}^t v[g, s, q] <= u[g, s, t]
@@ -127,7 +128,8 @@ def create_formulation(instance, lower_bound=None, relax=True):
     for g in Gf:
         DTg = int(DT[g])
         for t in range(0, N-DTg-1):
-            problem += (np.sum(v[g, :, t+1:t+DTg+1], axis=1) <= 1 - u[g, :, t])
+            if t + 1 < T:
+                problem += (np.sum(v[g, :, t+1:t+DTg+1], axis=1) <= 1 - u[g, :, t])
     
     # Define constraints group 3.33
     #    z[g, t] <= 1 for slow generators

src/utils.py
@@ -95,20 +95,27 @@ class SUCLpProblem(pulp.LpProblem):
         self.current_group_name = "-"
     
     def __iadd__(self, other):
+        assert(other is not False)
+        if other is True:
+            return True
         if isinstance(other, np.ndarray):
             result = self
             for index in np.ndindex(*other.shape):
                 self.groups.append(self.current_group_name)
-                result = super(SUCLpProblem, self).__iadd__(other[index])
+                result = self.add_single_constraint(other[index])
             if len(tuple(other.shape)) == 0:
                 self.last_constraint_mat_shape = (1,)
             else:
                 self.last_constraint_mat_shape = tuple(other.shape)
         else:
             self.groups.append(self.current_group_name)
-            result = super(SUCLpProblem, self).__iadd__(other)
+            result = self.add_single_constraint(other)
             self.last_constraint_mat_shape = (1,)
         return result
+    
+    def add_single_constraint(self, constraint):
+        assert(not isinstance(constraint, np.bool_))
+        return super(SUCLpProblem, self).__iadd__(constraint)
     
     def is_integer_solution(self, eps=1e-04):
         for variable in self.variables():